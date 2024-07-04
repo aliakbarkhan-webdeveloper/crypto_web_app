8423b95ebdd98e26aa3fcb1ac752ee40e464f20d
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-04 21:16:20 +0500

    main edited

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { PORT } = require("./config/config.js");
 const dbConnect = require("./mongoDB/dbConnect.js");
 const userRoute = require("./routes/user.route.js");
 const errorHandler = require("./middleware/errorHandler.middle.js");
-//By Ali
+//By Ali Akbar
 app.use(express.json());
 app.use(userRoute);
 dbConnect();
