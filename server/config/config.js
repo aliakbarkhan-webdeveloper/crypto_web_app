35cbb161bccaf5301bc36984d61d5f75dbf7ffae
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-04 18:19:00 +0500

    ,

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv=require("dotenv");
-dotenv.config();
-
-const PORT=process.env.PORT;
-const CONNECTION_STRING=process.env.CONNECTION_STRING;
-
-module.exports={
-    PORT,CONNECTION_STRING
-}
