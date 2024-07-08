579a3316b819505fff44a9a9553dc28f73940fd4
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-08 11:56:52 +0500

    by branch

--- server/services/JWTService.service.js
+++ server/services/JWTService.service.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
-
-class JWTService{
-    //sign/create tokken
-    //sign/create refresh tokken
+class JWTService {
+  //sign/create tokken
+  //sign/create refresh tokken
+  //verify access token
+  //verify refresh token
+  //store refresh token
 }
