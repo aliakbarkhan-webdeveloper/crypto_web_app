1c4dc77f8bf42a775d33a3345ddd2cff7e18a694
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-08 14:44:33 +0500

    DB modified

--- server/mongoDB/dbConnect.js
+++ server/mongoDB/dbConnect.js
@@ -1,16 +1,16 @@
-const mongoose=require("mongoose");
-const {CONNECTION_STRING}=require("../config/config.js");
+const mongoose = require("mongoose");
+const { CONNECTION_STRING } = require("../config/config.js");
 
-const dbConnect=async ()=>{
-    try {mongoose.set("strictQuery",false)
-        let connected=await mongoose.connect(CONNECTION_STRING);
-        console.log(`Data_BASE is connected at: ${connected.connection.host}`);
-        
-    } catch (error) {
-        console.log(error);
-    }
-    
+const dbConnect = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    let connected = await mongoose.connect(CONNECTION_STRING);
+    console.log(
+      `Data_BASE is connected at: ${connected.connection.host + " of mongooDB"}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
-
-module.exports=dbConnect;
+module.exports = dbConnect;
