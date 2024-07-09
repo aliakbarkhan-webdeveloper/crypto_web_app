a5234fb162776c3c0be4360262c4da075bd24edf
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-09 21:04:50 +0500

    finding error for token not working

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,13 @@
-const dotenv=require("dotenv");
-dotenv.config();
+const dtenv = require("dotenv");
+dtenv.config();
 
-const PORT=process.env.PORT;
-const CONNECTION_STRING=process.env.CONNECTION_STRING;
-const SECRET_KEY_ACCESS_TOKEN=process.env.SECRET_KEY_ACCESS_TOKEN;
-const SECRET_KEY_REFRESH_TOKEN=process.env.SECRET_KEY_REFRESH_TOKEN;
-module.exports={
-    PORT,CONNECTION_STRING,SECRET_KEY_ACCESS_TOKEN,SECRET_KEY_REFRESH_TOKEN
-}
+const PORT = process.env.PORT;
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const SECRET_KEY_ACCESS_TOKEN = process.env.SECRET_KEY_ACCESS_TOKEN;
+const SECRET_KEY_REFRESH_TOKEN = process.env.SECRET_KEY_REFRESH_TOKEN;
+module.exports = {
+  PORT,
+  CONNECTION_STRING,
+  SECRET_KEY_ACCESS_TOKEN,
+  SECRET_KEY_REFRESH_TOKEN,
+};
--- server/services/JWTService.service.js
+++ server/services/JWTService.service.js
@@ -1,36 +1,32 @@
 const jwt = require("jsonwebtoken");
 //importing Model to Store Refresh token
 const refreshTokenModel = require("../models/token.model.js");
-const {
-  SECRET_KEY_ACCESS_TOKEN,
-  SECRET_KEY_REFRESH_TOKEN,
-} = require("../config/config.js");
-let a="8648b97e722f0d440a1604d6cedc2487c7db4f65b12db8bdf4a568c3680960fbea5836f62fe9319f8eb3772dfeb751ca75f248f7b34fb3bca2ea27be9c6a4e39"
-let b="486d71e848aefb0afed16a5c425d710d8dfbfedea7bc5eaa4cd2844635d6d84c718365e802dd7c1ae687e9283cf69f28be51b67a9ce870c6815ccee7ef0978dd"
+const { SECRET_KEY_ACCESS_TOKEN } = require("../config/config.js");
+const { SECRET_KEY_REFRESH_TOKEN } = require("../config/config.js");
 
 class JWTService {
   //sign/create tokken
   //note make methods as static so we should not make new object each time for using methods
   static signAccessToken(payload, expiresTime) {
-    jwt.sign(payload,a,{ expiresIn: expiresTime });
+    jwt.sign(payload, SECRET_KEY_ACCESS_TOKEN, { expiresIn: expiresTime });
   }
   //sign/create refresh tokken
   static signRefreshToken(payload, expiresTime) {
-    jwt.sign(payload, b, { expiresIn: expiresTime });
+    jwt.sign(payload, SECRET_KEY_REFRESH_TOKEN, { expiresIn: expiresTime });
   }
   //verify access token
-  static verifyAccessToken(token) {
-    return jwt.verify(token, a);
+  static verifyAccessToken(Token) {
+    return jwt.verify(Token, SECRET_KEY_ACCESS_TOKEN);
   }
   //verify refresh token
-  static verifyRefreshToken(token) {
-    return jwt.verify(token, b);
+  static verifyRefreshToken(Token) {
+    return jwt.verify(Token, SECRET_KEY_REFRESH_TOKEN);
   }
   //store refresh token
-  static async refreshSave(token, userId) {
+  static async refreshSave(Token, userId) {
     try {
       const newToken = new refreshTokenModel({
-        token: token,
+        token: Token,
         userId: userId,
       });
       await newToken.save();
