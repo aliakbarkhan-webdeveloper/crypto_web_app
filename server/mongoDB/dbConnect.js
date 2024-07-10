686653f9f79ca830f47a9a5891e4512536d8eb30
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-10 15:09:11 +0500

    jwt are swnding to user while signup and saving refresh token via cookies

--- server/mongoDB/dbConnect.js
+++ server/mongoDB/dbConnect.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
-// const { CONNECTION_STRING } = require("../config/config.js");
-const CONNECTION_STRING="mongodb://localhost:27017";
+// const { CONNECTION_STRINGS } = require("../config/config.js");
+const CONNECTION_STRINGS = "mongodb://localhost:27017";
 const dbConnect = async () => {
   try {
     mongoose.set("strictQuery", false);
-    let connected = await mongoose.connect(CONNECTION_STRING);
-    console.log(
-      `Data_BASE is connected at: ${connected.connection.host + " of mongooDB"}`
-    );
+    const connect = await mongoose.connect(CONNECTION_STRINGS);
+    console.log(`Data_BASE is connected`);
   } catch (error) {
     console.log(error);
   }
--- server/services/JWTService.service.js
+++ server/services/JWTService.service.js
@@ -1,31 +1,35 @@
 const jwt = require("jsonwebtoken");
 //importing Model to Store Refresh token
 const refreshTokenModel = require("../models/token.model.js");
-// const { SECRET_KEY_ACCESS_TOKENS } = require("../config/config.js");
-// const { SECRET_KEY_REFRESH_TOKENS } = require("../config/config.js");
-const SECRET_KEY_ACCESS_TOKENS="8648b97e722f0d440a1604d6cedc2487c7db4f65b12db8bdf4a568c3680960fbea5836f62fe9319f8eb3772dfeb751ca75f248f7b34fb3bca2ea27be9c6a4e39";
-const SECRET_KEY_REFRESH_TOKENS="486d71e848aefb0afed16a5c425d710d8dfbfedea7bc5eaa4cd2844635d6d84c718365e802dd7c1ae687e9283cf69f28be51b67a9ce870c6815ccee7ef0978dd";
+const { SECRET_KEY_ACCESS_TOKENS,SECRET_KEY_REFRESH_TOKENS } = require("../config/config.js");
+
+// const SECRET_KEY_ACCESS_TOKENS="8648b97e722f0d440a1604d6cedc2487c7db4f65b12db8bdf4a568c3680960fbea5836f62fe9319f8eb3772dfeb751ca75f248f7b34fb3bca2ea27be9c6a4e39";
+// const SECRET_KEY_REFRESH_TOKENS="486d71e848aefb0afed16a5c425d710d8dfbfedea7bc5eaa4cd2844635d6d84c718365e802dd7c1ae687e9283cf69f28be51b67a9ce870c6815ccee7ef0978dd";
 
 class JWTService {
   //sign/create tokken
   //note make methods as static so we should not make new object each time for using methods
   signAccessToken(payload, expiresTime) {
-   return jwt.sign(payload, SECRET_KEY_ACCESS_TOKENS, { expiresIn: expiresTime });
+    return jwt.sign(payload, SECRET_KEY_ACCESS_TOKENS, {
+      expiresIn: expiresTime,
+    });
   }
   //sign/create refresh tokken
   signRefreshToken(payload, expiresTime) {
-    return jwt.sign(payload, SECRET_KEY_REFRESH_TOKENS, { expiresIn: expiresTime });
+    return jwt.sign(payload, SECRET_KEY_REFRESH_TOKENS, {
+      expiresIn: expiresTime,
+    });
   }
   //verify access token
- verifyAccessToken(token) {
+  verifyAccessToken(token) {
     return jwt.verify(token, SECRET_KEY_ACCESS_TOKENS);
   }
   //verify refresh token
-verifyRefreshToken(token) {
+  verifyRefreshToken(token) {
     return jwt.verify(token, SECRET_KEY_REFRESH_TOKENS);
   }
   //store refresh token
- async refreshSave(token, userid) {
+  async refreshSave(token, userid) {
     try {
       const newToken = new refreshTokenModel({
         token: token,
