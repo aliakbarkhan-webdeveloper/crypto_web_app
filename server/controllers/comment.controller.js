906aedeb15d4c79eb903f5f692afe4d2b4678993
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-23 16:34:34 +0500

    started working on the comments

---
+++ server/controllers/comment.controller.js
@@ -0,0 +1,10 @@
+
+
+let commentCreate=async ()=>{}
+
+
+
+//controller to get all the comments of a commentor
+let getById=async ()=>{}
+
+modulte.exports={commentCreate,getById}
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,4 +1,10 @@
-//comment
+const router = require("express").Router();
+const {
+  commentCreate,
+  getById,
+} = require("../controllers/comment.controller.js");
 //create
+router.post();
 //read
+router.get();
 //read comment by blog id
