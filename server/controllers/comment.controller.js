a2384ba0f30edaf88a772dc5d219d59e30baec98
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-23 17:34:44 +0500

    comment creation controller done

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,10 +1,42 @@
+const joi = require("joi");
+const mongodbIdPattern = /^[0-9a-fA-F]{24}$/;
+const commentModel = require("../models/comments.model.js");
 
 
-let commentCreate=async ()=>{}
+//controller for comment creation
+let commentCreate = async (req, res, next) => {
+  const createCommentSchema = joi.object({
+    content: joi.string().required(),
+    author: joi.string().regex(mongodbIdPattern).required(),
+    blog: joi.string().regex(mongodbIdPattern).required(),
+  });
 
+  const { error } = createCommentSchema.validate(req.body);
 
+  if (error) {
+    return next(error);
+  }
 
+  const { content, author, blog } = req.body;
+  try {
+    const newComment = new commentModel({
+      content,
+      author,
+      blog,
+    });
+    await newComment.save();
+  } catch (error) {
+    return next(error);
+  }
+  return res.status(201).json({
+    message: "comment created",
+  });
+};
 //controller to get all the comments of a commentor
-let getById=async ()=>{}
+let getById = async (req, res, next) => {
 
-modulte.exports={commentCreate,getById}
+
+
+};
+
+modulte.exports = { commentCreate, getById };
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const auth = require("../middleware/auth.middleware");
 const {
   commentCreate,
   getById,
 } = require("../controllers/comment.controller.js");
 //create
-router.post();
+router.post("/comment",auth,commentCreate);
 //read
-router.get();
+router.get("/comment/:id",auth,commentCreate);
 //read comment by blog id
