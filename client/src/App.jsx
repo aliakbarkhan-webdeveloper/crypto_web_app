cd7de2c236345d69a457fc04f4fafc89f990cbf5
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-05 20:59:08 +0500

    App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./assets/components/Home/Home.jsx";
+import Footer from "./assets/components/Footer/Footer.jsx";
 import Navbar from "./assets/components/navbar/Navbar.jsx";
-
+import "./App.css"
 
 
 function App() {
  
   return(
-  <div>
-    <Navbar/>
-  <h1>Ali Akbar Khan</h1>
-<p>is this working</p>
+  <div className="container">
+<BrowserRouter>
+
+<div className="layout">
+  <Navbar/>
+<Routes>
+  <Route path="/" exact element={<div className="main"><Home/></div>}/>
+  <Route path="/home" exact element={<div className="main"><Home/></div>}/>
+</Routes>
+  <Footer/>
+</div>
+
+</BrowserRouter>
 
 
   </div>)
