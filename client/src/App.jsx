d8a1ba0afa3bfed472c26d91512ab15ddb362194
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-06 20:11:06 +0500

    App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,40 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Home from "./assets/components/Home/Home.jsx";
 import Footer from "./assets/components/Footer/Footer.jsx";
 import Navbar from "./assets/components/navbar/Navbar.jsx";
 
 function App() {
- 
-  return(
-  <div className="container">
-<BrowserRouter>
-
-<div className="layout">
-  <Navbar/>
-<Routes>
-  <Route path="/" exact element={<div className="main"><Home/></div>}/>
-  <Route path="/home" exact element={<div className="main"><Home/></div>}/>
-</Routes>
-  <Footer/>
-</div>
-
-</BrowserRouter>
-
-
-  </div>)
-  }
+  return (
+    <div className='container'>
+      <BrowserRouter>
+        <div className='layout'>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              exact
+              element={
+                <div className='main'>
+                  <Home />
+                </div>
+              }
+            />
+            <Route
+              path='/home'
+              exact
+              element={
+                <div className='main'>
+                  <Home />
+                </div>
+              }
+            />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
